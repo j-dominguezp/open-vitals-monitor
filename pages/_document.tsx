b1a4db401221926526d77c3e003e6b44fd087d29
Juan Dominguez <j.dominguezp@pm.me>
2022-04-10 19:28:41 -0400

    style(document): fix prettier printWidth

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,10 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet" />
       </Head>
 
       <body>
