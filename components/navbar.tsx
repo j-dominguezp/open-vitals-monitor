4f3760976fa581204908a5a0d4fd1cf65ba0776e
Juan Dominguez <j.dominguezp@pm.me>
2022-04-10 19:27:02 -0400

    feat(navbar): add theme switch to navbar, replaced outlines with shadow rings

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,26 +1,25 @@
 import Link from 'next/link';
 import { FaGithub, FaHeartbeat } from 'react-icons/fa';
+import ThemeSwitcher from './ThemeSwitcher';
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-white/30 backdrop-blur-md py-6">
+    <nav className="w-full bg-transparent backdrop-blur-md py-6">
       <div className="container mx-auto flex items-center justify-between">
         <Link href="/" passHref>
-          <a className="inline-flex gap-x-4">
-            <FaHeartbeat className="text-3xl text-rose-500" />
+          <a className="inline-flex items-center gap-x-4">
+            <FaHeartbeat className="text-3xl text-rose-500 dark:text-rose-400" />
 
-            <h1 className="text-2xl font-semibold">Open Vitals Monitor</h1>
+            <h1 className="text-2xl text-rose-500 dark:text-rose-400 font-semibold">Open Vitals Monitor</h1>
           </a>
         </Link>
 
-        <div>
-          <a
-            href="https://github.com/j-dominguezp/open-vitals-monitor"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGithub className="text-3xl" />
+        <div className="flex items-center gap-x-6">
+          <a href="https://github.com/j-dominguezp/open-vitals-monitor" target="_blank" rel="noopener noreferrer">
+            <FaGithub className="text-2xl text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-100 " />
           </a>
+
+          <ThemeSwitcher />
         </div>
       </div>
     </nav>
