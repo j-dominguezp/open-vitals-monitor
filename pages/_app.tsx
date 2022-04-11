bc7c1cb2b067fd74492ae099e7678938a417be28
Juan Dominguez <j.dominguezp@pm.me>
2022-04-10 20:32:27 -0400

    fix(theme): add inline script to detect user preferred theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-import Script from 'next/script';
 import Navbar from '../components/Navbar';
 import Seo from '../components/Seo';
 
@@ -8,8 +7,6 @@ import '../styles/globals.css';
 function OpenVitalsMonitorApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script src="/scripts/theme.js" strategy="beforeInteractive" />
-
       <Seo />
 
       <Navbar />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,22 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet" />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function initTheme() {
+                const theme = JSON.parse(localStorage.theme).state.theme;
+              
+                if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                  document.documentElement.classList.add('dark');
+                } else {
+                  document.documentElement.classList.remove('dark');
+                }
+              })();
+            `,
+          }}
+        />
       </Head>
 
       <body>
